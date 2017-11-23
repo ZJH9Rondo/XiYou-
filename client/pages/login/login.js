92c446203dfd36c90cc9c13f8e7bb3f3b46d8ea2
QiaoDing <tjoe18@126.com>
2017-11-23 16:42:53 +0800

    login page can send data to my host, without https

--- client/pages/login/login.js
+++ client/pages/login/login.js
@@ -8,48 +8,90 @@ Page({
     password: '',
     vercode: '',
     session: '',
-    verimg: ''
+    verimg: '',
+    name: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad (options) {
     this.getVerImg();
   },
 
+  login () {
+    let that = this;
+    console.log(that.data);
+    wx.request({
+      url: "http://www.tjoe18.cn:4100/login",
+      // url: 'https://fz1b9jc6.qcloud.la/weapp/vercode',
+      data: {
+        username: that.data.username,
+        password: that.data.password,
+        verCode: that.data.vercode,
+        session: that.data.session
+      },
+      method: 'GET',
+      success (res) {
+        console.log(res);
+        if(res.data.result.message == "login success"){
+          that.setData({
+            name: res.data.result.name
+          });
+          wx.showToast({
+            title: that.data.name + '同学你好',
+            icon: 'success',
+            duration: 5000
+          });
+        } else {
+          wx.showModal({
+            title: '登录失败',
+            content: '请检查所填写的信息',
+          })
+          that.getVerImg();
+        }
+      }
+    });
+   
+  },
+
   // ---------- 获取学号 ---------- //
-  usernameInput: function (e) {
+  usernameInput (e) {
     this.setData({
       username: e.detail.value
     });
   },
 
   // ---------- 获取密码 ---------- //
-  passwordInput: function (e) {
+  passwordInput (e) {
     this.setData({
       password: e.detail.value
     });
   },
 
   // ---------- 获取验证码 ---------- //
-  vercodeInput: function (e) {
+  vercodeInput (e) {
     this.setData({
       vercode: e.detail.value
     });
   },
 
   // ---------- 获取验证码 ---------- //
-  getVerImg: function () {
+  getVerImg () {
     let that = this;
     wx.request({
-      url: "www.tjoe18.cn:4100/vercode",
+      url: "http://www.tjoe18.cn:4100/verCode",
+      // url: 'https://fz1b9jc6.qcloud.la/weapp/vercode',
       method: 'GET',
       header: {
         "Content-Type": "applciation/json"
       },
-      success: function (res) {
+      success (res) {
         console.log(res);
+        that.setData({
+          verimg: res.data.result.verCode,
+          session: res.data.result.session
+        });
       }
     })
   }
---
+++ server/controllers/vercode.js
@@ -0,0 +1,3 @@
+const superagent = require('superagent');
+
+let otherHost = superagent.get('http://www.tjoe18.cn:4100/verCode');
